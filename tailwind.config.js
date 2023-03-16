1d03e9bd9d80436408562ffe675eca6a3074c2b0
steven <steven@metaflyer.co.kr>
2023-03-16 19:22:05 +0900

    init: reference https://candrajengger.medium.com/is-it-possible-to-combine-next-js-ant-design-tailwind-css-in-one-project-1ab12406999a

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx}",
+	],
+  theme: {
+    extend: {
+			fontFamily: {
+				montserrat: ['Montserrat', 'sans-serif']
+			}
+		},
+		colors: { // custom color palette
+      primary: '#441151',
+      secondary: '#EE85B5',
+      violet: '#883677',
+      congo: '##FF958C',
+      success: '#5FC790',
+      warning: '#FFA600',
+      danger: '#FF5630',
+      dark: '#2E3A44',
+      info: '#1CA7EC',
+      black: '#2E3A44',
+      grey1: '#A0AABF',
+      grey2: '#C0C6D4',
+      grey3: '#E3E8F1',
+      light: '#F9FBFC',
+      white: '#FFF'
+    }
+  },
+  plugins: [],
+}
