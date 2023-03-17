fd24b4bb5afc4f9e43bde3edeb49513935ce32fd
steven <steven@metaflyer.co.kr>
2023-03-17 19:03:46 +0900

    update: https://www.devkkiri.com/post/56578a18-d1fc-4c67-a2c4-6d64e21cf70c

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    arrowParens: 'always',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    jsxSingleQuote: false,
+    printWidth: 120,
+    quoteProps: 'as-needed',
+    semi: true,
+    singleQuote: true,
+    useTabs: false,
+    tabWidth: 2,
+    trailingComma: 'es5',
+    endOfLine: 'auto',
+};
